b442aad0d52f04aaa38e703a137e0534f70b70a9
TienLe37 <20020218@vnu.edu.vn>
2024-10-07 16:26:27 +0700

    refresh access token

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,6 +4,7 @@ const {
   generateAccessToken,
   generateRefreshToken,
 } = require('../middlewares/jwt');
+const jwt = require('jsonwebtoken');
 
 const register = asyncHandler(async (req, res) => {
   const { email, password, firstname, lastname } = req.body;
@@ -40,7 +41,7 @@ const login = asyncHandler(async (req, res) => {
     const { password, role, ...userData } = response.toObject();
     // tao accessToken: Xác thực và phân quyền người dùng
     const accessToken = generateAccessToken(response._id, role);
-    // tao refreshToken : Tạo mới accessToken
+    // tao refreshToken : Dùng để tạo mới accessToken khi accessToken đã hết hạn
     const refreshToken = generateRefreshToken(response._id);
     // Luu refreshToken vao Database
     await User.findByIdAndUpdate(response._id, { refreshToken }, { new: true });
@@ -61,14 +62,38 @@ const login = asyncHandler(async (req, res) => {
 // Get User current
 const getCurrent = asyncHandler(async (req, res) => {
   const { _id } = req.user;
-  const user = await User.findById(_id);
+  const user = await User.findById(_id).select(
+    '-refreshToken -password -role '
+  );
   return res.status(200).json({
     success: user ? true : false,
     rs: user ? user : 'User not found',
+  });
+});
+// refreshAcessToken : dùng refreshToken để tạo accessToken mới
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  // Lay token tu cookies
+  const cookie = req.cookies;
+  // Kiểm tra xem có tokentrong cookies không
+  if (!cookie && !cookie.refreshToken)
+    throw new Error('No refreshToken in cookies');
+  // Kiem tra token còn hạn hay không rs: result
+  const rs = jwt.verify(cookie.refreshToken, process.env.JWT_SECRET);
+  // tìm kiếm user : findOne :  so sánh id với rs.id | refreshToken lưu trong database với lưu trên cookies có match không
+  const response = await User.findOne({
+    _id: rs._id,
+    refreshToken: cookie.refreshToken,
+  });
+  return res.status(200).json({
+    success: response ? true : false,
+    newAccessToken: response
+      ? generateAccessToken(response._id, response.role)
+      : ' refreshToken not match',
   });
 });
 module.exports = {
   register,
   login,
   getCurrent,
+  refreshAccessToken,
 };
