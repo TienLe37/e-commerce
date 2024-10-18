24d6651b985071600d2a13c1e67e71cd0f4cbd04
TienLe37 <20020218@vnu.edu.vn>
2024-10-18 22:01:25 +0700

    insert data to database

---
+++ data/cate_brand.js
@@ -0,0 +1,35 @@
+const data = [
+  {
+    cate: 'Smartphone',
+    brand: ['Apple', 'Samsung', 'LG', 'Asus', 'Nokia', 'BlackBerry'],
+  },
+  {
+    cate: 'Tablet',
+    brand: ['Ipad', 'Samsung', 'Acer', 'Asus', 'Lenovo', 'Haier'],
+  },
+  {
+    cate: 'Laptop',
+    brand: ['Dell', 'HP', 'Acer', 'Asus', 'Lenovo', 'Macbook'],
+  },
+  {
+    cate: 'Accessories',
+    brand: [
+      'Smatrswatchs',
+      'Headphone',
+      'Bluetooth',
+      'Keyboard',
+      'Mouse',
+      'Cases',
+    ],
+  },
+  {
+    cate: 'Television',
+    brand: ['Apple', 'Samsung', 'LG', 'Asus', 'Nokia', 'BlackBerry'],
+  },
+  {
+    cate: 'Printer',
+    brand: ['Samsung', 'Acer', 'Asus', 'Lenovo', 'Haier'],
+  },
+];
+
+module.exports = data;
